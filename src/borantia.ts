d9208f75ea8b8ec29890bb14cedb17288384c53f
Michael Zhou <zmichael105@gmail.com>
2022-06-05 07:48:34 -0700

    ff

--- src/borantia.ts
+++ src/borantia.ts
@@ -1,6 +1,5 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
-  Borantia,
   ApprovalForAll,
   Initialized,
   OwnershipTransferred,
@@ -8,62 +7,12 @@ import {
   TransferBatch,
   TransferSingle,
   URI,
-  Unpaused
-} from "../generated/Borantia/Borantia"
-import { ExampleEntity } from "../generated/schema"
+  Unpaused,
+  ClaimedVolunteerHours,
+} from "../generated/Borantia/Borantia";
+import { BorantiaVolunteer } from "../generated/schema";
 
-export function handleApprovalForAll(event: ApprovalForAll): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.balanceOfBatch(...)
-  // - contract.getMetadata(...)
-  // - contract.getVolunteerList(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.latestUnusedTokenId(...)
-  // - contract.owner(...)
-  // - contract.paused(...)
-  // - contract.supportsInterface(...)
-  // - contract.tokenIdToMetadata(...)
-  // - contract.tokenIdToVolunteers(...)
-  // - contract.uri(...)
-}
+export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleInitialized(event: Initialized): void {}
 
@@ -78,3 +27,11 @@ export function handleTransferSingle(event: TransferSingle): void {}
 export function handleURI(event: URI): void {}
 
 export function handleUnpaused(event: Unpaused): void {}
+
+export function handleClaimedVolunteerHours(event: ClaimedVolunteerHours): void {
+  let bv = new BorantiaVolunteer(event.transaction.hash.toString())
+  bv.xp = event.params.h;
+  bv.tokenId = event.params.tid;
+  bv.account = event.params.volunteer;
+  bv.save();
+}
